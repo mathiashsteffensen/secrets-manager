a5e7fa7f7b9409840fb578cd4e8058a0eb980d0e
mathias <ms@driversnote.com>
2021-09-07 19:37:13 +0200

    Rewrite accessors_test to use testify/assert

--- app_config/accessors_test.go
+++ app_config/accessors_test.go
@@ -17,67 +17,32 @@ limitations under the License.
 package AppConfig
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
+var _ = LoadEncrypted("../config/secrets.yml.enc", "../config/master.key")
+
 func TestGet(t *testing.T) {
-	err := LoadEncrypted("../config/secrets.yml.enc", "../config/master.key")
-
-	if err != nil {
-		t.Errorf("Unexpected error when loading encrypted secrets %#v", err)
-	}
-
 	got, err := Get("secret")
-
-	if err != nil {
-		t.Errorf("Unexpected error when running Get %#v", err)
-	}
-
-	if got.(string) != "hello" {
-		t.Errorf(`Expected Get("secret") to equal "hello" but got %#v`, got.(string))
-	}
+	assert.Nil(t, err)
+	assert.Equal(t, "hello", got)
 
 	got, err = Get("super.deeply.nested")
-
-	if err != nil {
-		t.Errorf("Unexpected error when running Get %#v", err)
-	}
-
-	if got.(string) != "value" {
-		t.Errorf(`Expected Get("super.deeply.nested") to equal "value" but got %#v`, got.(string))
-	}
+	assert.Nil(t, err)
+	assert.Equal(t, got, "value")
 }
 
 func TestExists(t *testing.T) {
-	err := LoadEncrypted("../config/secrets.yml.enc", "../config/master.key")
-
-	if err != nil {
-		t.Errorf("Unexpected error when loading encrypted secrets %#v", err)
-	}
-
 	got := Exists("secret")
-
-	if got != true {
-		t.Errorf("Expected key 'secret' to exists but it didn't")
-	}
+	assert.Equal(t, true, got)
 
 	got = Exists("this.is.not.a.real.key")
-
-	if got != false {
-		t.Errorf("Expected key 'secret' to not exists but it did")
-	}
+	assert.Equal(t, false, got)
 }
 
 func TestAllKeys(t *testing.T) {
-	err := LoadEncrypted("../config/secrets.yml.enc", "../config/master.key")
-
-	if err != nil {
-		t.Errorf("Unexpected error when loading encrypted secrets %#v", err)
-	}
-
 	for _, s := range AllKeys() {
-		if !Exists(s) {
-			t.Errorf("Expected key %s to exist but it didn't", s)
-		}
+		assert.Equal(t, true, Exists(s))
 	}
 }
